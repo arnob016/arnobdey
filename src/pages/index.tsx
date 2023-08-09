2d020e9bfb60a9effeada53af519cf895634fe92
arnob016 <arnob.social@gmail.com>
2023-08-09 11:27:38 +0600

    image update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from 'next/image';
 import Link from "next/link";
 
 export default function Home() {
@@ -10,8 +11,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#161616] to-[#2d2d2d]">
-        <img src="https://shadowblood.gallerycdn.vsassets.io/extensions/shadowblood/tailwind-moon/3.0.2/1673948732518/Microsoft.VisualStudio.Services.Icons.Default" className="max-h-28 rounded-full"/> 
-        <h1 className="text-5xl font-bold text-white">Arnob Dey</h1>
+      <div className="rounded-full overflow-hidden">
+        <Image src="/beach.jpg" alt="Basic image" width={260} height={260} style={{ borderRadius: '50%' }}/>
+      </div>
+       <h1 className="text-5xl font-bold text-white">Arnob Dey</h1>
       </main>
     </>
   );
